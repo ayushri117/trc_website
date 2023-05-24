2387270e028b72685ddc74cdccb76c70639730ca
unknown <prathameshjondhale100@gmail.com>
2023-05-24 20:36:02 +0530

    Added Contact

--- Frontend/trc-frontend/src/App.jsx
+++ Frontend/trc-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero/Hero";
 import AboutUs from "./Components/AboutUs/AboutUs";
 import Team from "./Components/Team/Team";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero></Hero>
       <AboutUs></AboutUs>
       <Team></Team>
+      <Contact></Contact>
     </div>
   );
 }
--- Frontend/trc-frontend/src/Components/Contact/Contact.jsx
+++ Frontend/trc-frontend/src/Components/Contact/Contact.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Contact.css";
+import {
+  Facebook,
+  Instagram,
+  Linkedin,
+  Mail,
+} from "feather-icons-react/build/IconComponents";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact_container">
+      {/* <h2 className="contact_heading">CONTACT</h2> */}
+      <div className="contact_box">
+        <div className="contact_socialMedia">
+          <Facebook className="contact_linkIcon"></Facebook>
+          <Linkedin className="contact_linkIcon"></Linkedin>
+          <Instagram className="contact_linkIcon"></Instagram>
+          <Mail className="contact_linkIcon"></Mail>
+        </div>
+        <p className="footer_text">Copyright © Robotics Club, IIT Palakkad</p>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
