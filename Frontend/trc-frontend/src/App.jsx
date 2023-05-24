2d3c7362ca1d1f7cd205087ede3eba3c5681f934
unknown <prathameshjondhale100@gmail.com>
2023-05-24 18:33:49 +0530

    Added Hero

--- Frontend/trc-frontend/src/App.jsx
+++ Frontend/trc-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Hero></Hero>
     </div>
   );
 }
--- Frontend/trc-frontend/src/Components/Hero/Hero.jsx
+++ Frontend/trc-frontend/src/Components/Hero/Hero.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import bgVideo from "../../assets/bgVideo.mp4";
 import "./Hero.css";
 
 const Hero = () => {
-  return <div className="hero_container">Hero</div>;
+  return (
+    <div className="hero_container">
+      <div className="hero_overlay"></div>
+      <video src={bgVideo} autoPlay loop muted></video>
+      <div className="hero_content">
+        <h1 className="hero_heading">THE ROBOTICS CLUB</h1>
+        <h3 className="hero_subheading">IIT PALAKKAD</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
