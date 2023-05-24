f500dbdd0aa6cdd8e55782253d14ec289fdad8e7
unknown <prathameshjondhale100@gmail.com>
2023-05-24 19:06:55 +0530

    Added About Us

--- Frontend/trc-frontend/src/App.jsx
+++ Frontend/trc-frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero/Hero";
+import AboutUs from "./Components/Hero/AboutUs/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Hero></Hero>
+      <AboutUs></AboutUs>
     </div>
   );
 }
---
+++ Frontend/trc-frontend/src/Components/Hero/AboutUs/AboutUs.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./AboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <div className="about_container">
+      <h1 className="about_heading">ABOUT US</h1>
+      <p className="about_content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nam
+        tempora, eos pariatur neque cumque commodi nostrum, sint suscipit libero
+        delectus atque veritatis aspernatur incidunt nemo dicta voluptates at
+        sequi facilis ducimus iste! Optio quae, nemo quasi reiciendis sint odit,
+        veniam fuga corporis delectus molestiae totam quaerat modi sapiente
+        distinctio. Corporis harum quidem libero tempora nobis repellat possimus
+        ipsum veniam rerum aspernatur? Optio, molestias? Hic at porro
+        exercitationem dolorum earum quia odio, aperiam minus sint aspernatur
+        sapiente maiores iste magni recusandae necessitatibus omnis expedita
+        quae doloremque voluptas. Corporis numquam cum quis? Corporis inventore
+        adipisci rerum libero, architecto magnam quod quis? Culpa impedit
+        deleniti natus labore. Recusandae itaque dolorem amet quisquam assumenda
+        ipsam saepe sequi enim quia. Blanditiis sint ducimus pariatur rerum enim
+        nihil vitae aliquam labore iusto voluptatibus. Ullam provident ipsum
+        commodi eaque alias quisquam beatae quaerat deserunt incidunt, porro ex.
+        Neque facere tempora dolorum voluptatem sit corporis odit quia labore
+        asperiores atque placeat veniam, cupiditate blanditiis fuga autem? Rem
+        corporis id molestiae. Ratione aliquam voluptatem maiores velit,
+        repellat ipsa omnis enim quisquam, nulla, consequatur eius! Illum labore
+        corrupti eos quae esse ipsum, fugit magnam voluptate quo nihil
+        veritatis, minima molestias! Molestiae eius doloribus unde veniam in eum
+        modi iste.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
