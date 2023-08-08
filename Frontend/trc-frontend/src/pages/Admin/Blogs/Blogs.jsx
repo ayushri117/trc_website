c2196adf9cc3816e4855b0bb85e24e57ec425a63
unknown <prathameshjondhale100@gmail.com>
2023-08-08 14:26:42 +0530

    Blog Filter for admin

--- Frontend/trc-frontend/src/pages/Admin/Blogs/Blogs.jsx
+++ Frontend/trc-frontend/src/pages/Admin/Blogs/Blogs.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Blog from "./Blog";
 import {
   defer,
@@ -8,6 +8,7 @@ import {
   json,
   redirect,
   useLoaderData,
+  useLocation,
 } from "react-router-dom";
 import { getAuthToken } from "../../../../util/auth";
 import axios from "axios";
@@ -15,18 +16,70 @@ import { AnimatePresence } from "framer-motion";
 
 const Blogs = () => {
   const data = useLoaderData();
-  console.log(data);
+  const [rescorceOption, setRecourceOption] = useState([]);
+  const [selectedResource, setSelectedResource] = useState();
 
   if (data.blogData.length === 0) {
     return <h2 style={{ color: "white" }}>No Blogs Here</h2>;
   }
+
+  useEffect(() => {
+    getResources();
+  }, []);
+
+  const getResources = async () => {
+    const headers = {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    };
+
+    const response = await axios.get(
+      "https://trc-iitpkd-backend.onrender.com/resource",
+      {
+        headers: headers,
+      }
+    );
+
+    let option = [];
+    for (let i of response.data.resource) {
+      let temp = {
+        value: i._id,
+        name: i.heading,
+      };
+      // console.log(temp);
+      option.push(temp);
+    }
+
+    setRecourceOption(option);
+  };
+
+  const checkResourceId = (blog) => {
+    return blog.resourceRef === selectedResource;
+  };
+
+  const changeSelectedResHandler = (e) => {
+    setSelectedResource(e.target.value);
+  };
   return (
-    <div className="admin_blogs">
-      <AnimatePresence>
-        {data.blogData.map((blog) => (
-          <Blog data={blog}></Blog>
+    <div className="admin_blogs_Box">
+      <select
+        name="resource"
+        id="resourceInput"
+        className="blog_dropdown"
+        onChange={changeSelectedResHandler}
+        style={{ width: "200px" }}
+      >
+        {rescorceOption.map((resource) => (
+          <option value={`${resource.value}`}>{resource.name}</option>
         ))}
-      </AnimatePresence>
+      </select>
+      <div className="admin_blogs">
+        <AnimatePresence>
+          {data.blogData.filter(checkResourceId).map((blog) => (
+            <Blog data={blog}></Blog>
+          ))}
+        </AnimatePresence>
+      </div>
     </div>
   );
 };
